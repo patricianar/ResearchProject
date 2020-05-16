07e54eccc5ec0eaa91ac21c948f3d6e6a0a6ff56
bayronPalacio <58010308+bayronPalacio@users.noreply.github.com>
2020-05-16 14:56:28 -0700

    Order class changes

--- app/src/main/java/com/example/researchproject/Classes/Order.java
+++ app/src/main/java/com/example/researchproject/Classes/Order.java
@@ -2,28 +2,17 @@ package com.example.researchproject.Classes;
 
 public class Order {
         private String cart_id;
-        private String credit_card_type;
         private Customer customer;
         private String customer_message;
         private String date_created;
         private String date_shipped;
         private int id;
-        private int items_shipped;
         private int items_total;
         private String payment_method;
         private String payment_status;
         private Products products;
-        private String shipping_cost_ex_tax;
-        private String shipping_cost_inc_tax;
-        private String shipping_cost_tax;
-        private int shipping_cost_tax_class_id;
+        private Double shipping_cost_tax;
         private String status;
-        private int status_id;
-        private String subtotal_ex_tax;
-        private String subtotal_inc_tax;
-        private String subtotal_tax;
-        private String tax_provider_id;
-        private String total_ex_tax;
-        private String total_inc_tax;
-        private String total_tax;
+        private Double total;
+        private Double total_tax;
 }
