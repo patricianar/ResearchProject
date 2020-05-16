f69d8c38161d7d273736d2853c853665e88f741a
Patricia Narvaez <patico832@hotmail.com>
2020-05-16 00:02:45 -0700

    customer class

---
+++ app/src/main/java/com/example/researchproject/Classes/Customer.java
@@ -0,0 +1,95 @@
+package com.example.researchproject.Classes;
+
+public class Customer
+{
+    private String city;
+    private String country;
+    private String email;
+    private String first_name;
+    private int id;
+    private String last_name;
+    private String phone;
+    private String postal_code;
+    private String state;
+    private String street_1;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet_1() {
+        return street_1;
+    }
+
+    public void setStreet_1(String street_1) {
+        this.street_1 = street_1;
+    }
+}
