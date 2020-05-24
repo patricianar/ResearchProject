fd78821aa3eea9ea19cfc4028e04df7599131aa6
Patricia Narvaez <patico832@hotmail.com>
2020-05-24 13:12:09 -0700

    admin - import file to read products

--- app/src/main/java/com/example/researchproject/Admin/ProductAdminActivity.java
+++ app/src/main/java/com/example/researchproject/Admin/ProductAdminActivity.java
@@ -3,38 +3,34 @@ package com.example.researchproject.Admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.researchproject.Classes.Catalogue;
-import com.example.researchproject.Classes.Customer;
 import com.example.researchproject.Classes.Product;
-import com.example.researchproject.Customer.LoginActivity;
 import com.example.researchproject.Customer.ProductCustomerActivity;
-import com.example.researchproject.Customer.RegistrationActivity;
 import com.example.researchproject.R;
 import com.example.researchproject.VolleyService;
 import com.google.gson.Gson;
 
+
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Objects;
+
 
 public class ProductAdminActivity extends AppCompatActivity {
     private static final String TAG = "ProductAdminActivity";
-    private static final int ACTIVITY_CHOOSE_FILE1 = 0;
+    private static final int FILE_REQUEST_CODE = 10;
+
+    private ArrayList<Product> productsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +41,31 @@ public class ProductAdminActivity extends AppCompatActivity {
         btnImport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ArrayList<Product> productsList = ReadCSV();
+                selectCSVFile();
+            }
+        });
+    }
+
+    private void selectCSVFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent, FILE_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                readCSVFromUri(uri);
 
                 String url = "https://myprojectstore.000webhostapp.com/product/";
 
@@ -60,95 +80,45 @@ public class ProductAdminActivity extends AppCompatActivity {
                     @Override
                     public void getResponse(String response) {
                         try {
-                            if(response.equals("true")){
+                            if (response.equals("true")) {
                                 Toast.makeText(ProductAdminActivity.this, "Your catalogue has been updated", Toast.LENGTH_SHORT).show();
                                 final Intent productIntent = new Intent(ProductAdminActivity.this, ProductCustomerActivity.class);
-                                Thread thread = new Thread() {
-                                    @Override
-                                    public void run() {
-                                        try {
-                                            Thread.sleep(3500); // wait before going to products
-                                            startActivity(productIntent);
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                };
-                                thread.start();
+//                                Thread thread = new Thread() {
+//                                    @Override
+//                                    public void run() {
+//                                        try {
+//                                            Thread.sleep(3500); // wait before going to products
+//                                            startActivity(productIntent);
+//                                        } catch (Exception e) {
+//                                            e.printStackTrace();
+//                                        }
+//                                    }
+//                                };
+//                                thread.start();
                             }
+                            productsList.clear();
                             Log.d(TAG, response);
                         } catch (Exception ex) {
                             Log.e(TAG, ex.getMessage());
                         }
                     }
                 }, "products", jsonCatalogue);
-
-                //selectCSVFile();
-            }
-        });
-    }
-
-    private void selectCSVFile() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
-        startActivityForResult(Intent.createChooser(intent, "Open CSV"), ACTIVITY_CHOOSE_FILE1);
-        Log.e("here", "a");
-
-//        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-//        intent.addCategory(Intent.CATEGORY_OPENABLE)
-//        intent.type = "text/*"
-//        startActivityForResult(intent, READ_REQUEST_CODE)
-
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case ACTIVITY_CHOOSE_FILE1: {
-                if (resultCode == RESULT_OK) {
-                    proImportCSV(new File(data.getData().getPath()));
-                    Log.e("here", "b");
-                }
             }
         }
     }
 
-    private void proImportCSV(File from) {
+    private void readCSVFromUri(Uri uri) {
         try {
-            // reading CSV and writing table
-            //CSVReader dataRead = new CSVReader(new FileReader(from)); // <--- This line is key, and why it was reading the wrong file
-            Log.e("here", "c");
-            //ReadCSV(from);
-        } catch (Exception e) {
-            Log.e("TAG", e.toString());
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)));
 
-        }
-    }
-
-    private ArrayList<Product> ReadCSV() {
-        ArrayList<Product> productsList = new ArrayList<>();
-
-        InputStream inputStream = getResources().openRawResource(R.raw.mock_data);//opening input stream with raw csv resource
-
-
-        try{
-            //String extr = Environment.getExternalStorageDirectory().toString();
-            //FileInputStream fileInputStream = new FileInputStream(extr + from);
-            BufferedReader reader =  new BufferedReader(new InputStreamReader(inputStream));
-            String csvLine;
-            if((csvLine =  reader.readLine()) != null){
-                //here csv Line contains the header line and can be used if needed
+            String line;
+            if ((line = reader.readLine()) != null) {
+//                //here csv Line contains the header line and can be used if needed
             }
-            while ((csvLine = reader.readLine()) != null){
-
-                String[] row = csvLine.split(",");
-
-                //SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-               // Date dateAuction = formatter.parse(row[0]);
-
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                Log.e("test", row[1]);
                 String barcode = row[0];
                 String brand = row[1];
                 String category = row[2];
@@ -162,20 +132,13 @@ public class ProductAdminActivity extends AppCompatActivity {
                 String urlImg = row[10];
 
                 Product product = new Product(barcode, brand, category, costPrice, description, id, invLevel, invWarnLevel, name, price, urlImg);
-
-
-
-
                 productsList.add(product);
             }
-        }catch (IOException ex){
+        } catch (IOException ex) {
             throw new RuntimeException("Error reading file..." + ex.getMessage());
-        }catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             throw new RuntimeException("Error in id parsing..." + ex.getMessage());
-        }//catch (ParseException ex){
-            //if date formatter results in an exception
-            //throw new RuntimeException("Error in parsing date..." + ex.getMessage());
-        //}
-        return productsList;
+        }
     }
+
 }
